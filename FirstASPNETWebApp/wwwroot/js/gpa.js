671f2e7d95d399ff43dd0117ec4491bc545407cd
Preston Loveland <62196792+prestonloveland@users.noreply.github.com>
2021-01-11 18:48:15 -0700

    Add project files.

---
+++ FirstASPNETWebApp/wwwroot/js/gpa.js
@@ -0,0 +1,46 @@
+﻿/*Preston Loveland
+Assignment 1 IS 414
+Group 11 Section 1 */
+function calculate_grade()
+{
+    //declare variables
+    var assignments = document.getElementById("assignments").value;
+    var group_projects = document.getElementById("group-project").value;
+    var quizzes = document.getElementById("quizzes").value;
+    var exams = document.getElementById("exams").value;
+    var intex = document.getElementById("intex").value;
+    var final_grade = 0;
+    var letter_grade = "";
+    //calculate grade
+    final_grade = (.50 * assignments) + (.10 * group_projects) + (.10 * quizzes) + (.20 * exams) + (.10 * intex);
+
+    //get letter grade
+    if (final_grade >= 94) {
+        letter_grade = "A";
+    } else if (final_grade >= 90) {
+        letter_grade = "A-";
+    } else if (final_grade >= 87) {
+        letter_grade = "B+";
+    } else if (final_grade >= 84) {
+        letter_grade = "B";
+    } else if (final_grade >= 80) {
+        letter_grade = "B-";
+    } else if (final_grade >= 77) {
+        letter_grade = "C+";
+    } else if (final_grade >= 74) {
+        letter_grade = "C";
+    } else if (final_grade >= 70) {
+        letter_grade = "C-";
+    } else if (final_grade >= 67) {
+        letter_grade = "D+";
+    } else if (final_grade >= 64) {
+        letter_grade = "D";
+    } else if (final_grade >= 60) {
+        letter_grade = "D-";
+    } else {
+        letter_grade = "E";
+    }
+
+    //send alert
+    alert("Final Grade: " + final_grade + " : " + letter_grade);
+}
